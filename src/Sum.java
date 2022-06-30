03c0c53ea0fa62fee1cb978b601fd359e4388b3c
seunghyubchoi <seunghyubchoi@localhost>
2022-06-29 22:24:14 -0700

    두 수의 합 구하기

---
+++ src/Sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		System.out.println("두 수를 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+//		String inputValue = scanner.nextLine();
+//		String[] splitedValue = inputValue.split(" ");
+//		int first = Integer.parseInt(splitedValue[0]);
+//      int second = Integer.parseInt(splitedValue[1]);
+	String s1,s2;
+	s1 = scanner.next();
+	s2 = scanner.next();
+	
+	int a = Integer.parseInt(s1);
+	int b = Integer.parseInt(s2);
+        
+//       System.out.println("두 수의 합은 " + (a + b) + "입니다.");
+	System.out.printf("%d와 %d의 합은 %d 입니다.", a, b, a + b);
+	scanner.close();
+	}
+}
