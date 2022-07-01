848b9a71ac6367a1078a362cb25117edf2b33680
seunghyubchoi <seunghyubchoi@localhost>
2022-06-30 18:42:54 -0700

    while

--- src/Repeat.java
+++ src/Repeat.java
@@ -2,14 +2,24 @@ import java.util.Scanner;
 
 public class Repeat {
 	public static void main(String[] args) {
-		int[] maxDays = {31, 28, 31, 30 , 31, 30, 31, 31, 30, 31, 30, 31};
+		String PROMPT = "cal> ";
+		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		System.out.println("반복횟수를 입력하세요.");
+
 		Scanner scanner = new Scanner(System.in);
-		int repeat = scanner.nextInt();
-		for(int i = 1; i <= repeat; i++) {
+	
+		
+		while (true) {
 			System.out.println("월을 입력하세요.");
+			System.out.print(PROMPT);
 			int month = scanner.nextInt();
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+			if (month == -1) {
+				System.out.println("잘 못 된 입력입니다.");
+				break;	
+			} else if (month > 12) {
+				continue;
+			}
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
 		}
 
 	}
