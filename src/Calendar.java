99c96b570e068c40e3370fd6ace5a5d8a5841589
seunghyubchoi <seunghyub.choi@gmail.com>
2022-07-09 09:10:37 -0700

    morning practice

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,11 +1,22 @@
-
+import java.util.Scanner;
 public class Calendar {
 	public static void main(String[] args) {
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.println("달을 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+		if(month >= 1 && month <= 12) {
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		} 
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("-----------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		
 	}
 }
