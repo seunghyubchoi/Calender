91b88fc80b7f730997895ea130d6f488d91cc5da
seunghyubchoi <seunghyub.choi@gmail.com>
2022-07-09 00:57:10 -0700

    next();는 공백을 포함 안한 문자 하나만을 받

--- src/Sum.java
+++ src/Sum.java
@@ -4,17 +4,12 @@ public class Sum {
 	public static void main(String[] args) {
 		
 		System.out.println("두 수를 입력하세요.");
-		Scanner scanner = new Scanner(System.in);
-		String s1, s2;
-		s1 = scanner.next();
-		s2 = scanner.next();
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
 		int a = Integer.parseInt(s1);
 		int b = Integer.parseInt(s2);
-		int c = a + b; 
-		System.out.printf("두 수의 합은 " + c + " 입니다.");
+		System.out.println("두 수의 합은 " + (a + b) + "입니다.");
 		
-		scanner.close();
-
-				
 	}
 }
