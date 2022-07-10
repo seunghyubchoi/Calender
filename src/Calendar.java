9afd5666a570c3c882050a8c725a161971e867c2
seunghyubchoi <seunghyub.choi@gmail.com>
2022-07-10 00:22:03 -0700

    for를 사용한 반복

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
-		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
-		System.out.println("달을 입력하세요.");
+
+		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		System.out.println("반복횟수를 입력하세요.");
 		Scanner sc = new Scanner(System.in);
-		int month = sc.nextInt();
-		if(month >= 1 && month <= 12) {
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
-		} 
+		int repeat = sc.nextInt();
+		for(int i = 1; i <= repeat; i++) {
+			System.out.println("월을 입력하세요.\n");
+			int month = sc.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month - 1]);
+			
+		}
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("-----------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -17,6 +21,8 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
-		
+
+
 	}
 }
+ 
